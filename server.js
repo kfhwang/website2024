473584e42961d5cbe9fe6438a2077c6a91d0b7b1
kfhwang <kfhwang@nutc.edu.tw>
2024-11-15 12:05:03 +0800

    20241115

---
+++ server.js
@@ -0,0 +1,33 @@
+//install: node js
+//install web server package: express >npm install express
+var express = require("express");
+var server = express();
+
+//web root
+server.use(express.static(__dirname+"/AgencyProject"));
+
+var DB = require("nedb-promises");
+var Profolio = DB.create(__dirname+"/profolio.db");
+
+Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+
+server.get("/services", (req, res)=>{
+    //DB find
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+    ];
+    res.send(Services);
+});
+
+server.get("/profolio", (req,res)=>{
+      //DB
+
+      res.send("Profolio");
+
+})
+
+
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})
